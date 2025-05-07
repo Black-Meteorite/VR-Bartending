522e829f722474879241bddb8853c0e2a8541790
Jai <jair16cs@gmail.com>
2025-05-06 19:26:14 -0700

    Drinks work

--- Assets/Jai Stuff/Scripts/ShakerCupLidAtatchment.cs
+++ Assets/Jai Stuff/Scripts/ShakerCupLidAtatchment.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
-
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using System.Collections.Generic;
 
 public class ShakerCupSnap : MonoBehaviour
 {
     public Transform lid;
     public Transform snapPoint;
+
     private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable lidGrab;
     private Rigidbody lidRb;
-
     private bool lidSnapped = false;
+
+    private InputDevice rightHandDevice;
 
     void Start()
     {
@@ -16,38 +20,68 @@ public class ShakerCupSnap : MonoBehaviour
         {
             lidGrab = lid.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
             lidRb = lid.GetComponent<Rigidbody>();
+            lidGrab.selectEntered.AddListener(OnLidGrabbed);
         }
+
+        TryInitializeRightHandDevice();
+    }
+
+    void Update()
+    {
+        // Reacquire if controller is not valid
+        if (!rightHandDevice.isValid)
+            TryInitializeRightHandDevice();
+
+        if (lidSnapped && rightHandDevice.isValid)
+        {
+            if (rightHandDevice.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerPressed) && triggerPressed)
+            {
+                ReleaseLid();
+            }
+        }
+    }
+
+    private void TryInitializeRightHandDevice()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, devices);
+        if (devices.Count > 0)
+            rightHandDevice = devices[0];
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!lidSnapped && other.transform == lid)
+        if (!lidSnapped && other.gameObject == lid.gameObject)
         {
-            lidOn();
+            SnapLid();
         }
     }
 
-    void lidOn()
+    private void SnapLid()
     {
         lid.position = snapPoint.position;
         lid.rotation = snapPoint.rotation;
 
         lidRb.isKinematic = true;
         lidRb.useGravity = false;
-
         lidGrab.enabled = false;
 
-        lidSnapped = true;
-
         lid.SetParent(transform);
+        lidSnapped = true;
     }
 
-    public void ReleaseLid()
+    private void ReleaseLid()
     {
+        lid.SetParent(null);
         lidRb.isKinematic = false;
         lidRb.useGravity = true;
         lidGrab.enabled = true;
         lidSnapped = false;
-        lid.SetParent(null);
+    }
+
+    private void OnLidGrabbed(SelectEnterEventArgs args)
+    {
+        if (lidSnapped)
+            ReleaseLid();
     }
 }
